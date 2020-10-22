f141fc1c86d9805dbc8846f27637a56d364407ac
dibyendughosh18 <dibyendughosh18@gmail.com>
2020-10-22 13:50:36 +0530

    initial commit

---
+++ src/main/java/com/bookstore/Bookstore60Application.java
@@ -0,0 +1,12 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookstore60Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bookstore60Application.class, args);
+	}
+}
